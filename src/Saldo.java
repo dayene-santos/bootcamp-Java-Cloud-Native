4ef6c113a75ab19738ac54de390db98e230c294b
DayeneS <dayene-santos@hotmail.com>
2025-03-30 20:34:50 -0300

    Desafio Calculo de Saldo

--- src/Saldo.java
+++ src/Saldo.java
@@ -1,9 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Saldo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+       
+    	Locale.setDefault(Locale.US);
+    	Scanner sc = new Scanner(System.in);
 
-	}
+    	System.out.print("Insira o saldo inicial: ");
+    	double saldoInicial = sc.nextDouble();
 
+    	System.out.println("Insira os valores das transações: ");
+        double valor1, valor2, valor3;
+        
+        valor1 = sc.nextDouble();
+        valor2 = sc.nextDouble();
+        valor3 = sc.nextDouble();
+
+        double saldoFinal = saldoInicial + valor1 + valor2 + valor3; 
+        
+        System.out.printf("%.2f\n", saldoFinal);
+
+        sc.close();
+    }
 }
+
