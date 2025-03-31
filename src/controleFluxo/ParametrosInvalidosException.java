b646d80980ceeb95736f0f1ee8e5b80ea5919cfb
DayeneS <dayene-santos@hotmail.com>
2025-03-30 22:06:06 -0300

    Desafio Tratamento de Exceção

---
+++ src/controleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+package controleFluxo;
+
+	public class ParametrosInvalidosException extends Exception {
+	 		
+	    public ParametrosInvalidosException(String mensagem) {
+	        super("O segundo parâmetro deve ser maior que o primeiro"); 
+	    }
+	}
